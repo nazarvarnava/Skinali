9554ec529e28533c4489be034fd965cfe8173815
unknown <nazar.varnava@gmail.com>
2020-07-23 23:27:44 +0300

    Add JS and bug fixed

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(window).load(function () {
+    $(".before-after").twentytwenty({
+        before_label: 'Без скинали',
+        after_label: 'Со скинали'
+    });
+    $('.before-slider').slick({
+        draggable: false,
+        dots: true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+    });
+    $('.menu-button').on('click', function () {
+        $('.menu').toggleClass('menu_active');
+    });
+
+
+    $('.select_checked').on('click', function (){
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+    $('.select__option').on('click', function(){
+        let value = $(this).attr('data-value');
+        $('#select-type').val(value);
+        $('.select_checked').text(value);
+        $('.select__dropdown').toggleClass('select__dropdown_open')
+    });
+    $("a[href^='#']").click(function () {
+        let _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top -100 + "px"});
+        return false;
+    });
+    $('input[type="tel"]').mask("+7 (999) 999-99-99");
+});
